3cb197a9ccf8d54275c628479ca3c6f207ef270f
Matt Couch <mcouch@gmail.com>
2025-05-24 13:10:06 -0500

    add level 6 and 7

--- scripts/jellyfish.gd
+++ scripts/jellyfish.gd
@@ -25,7 +25,7 @@ func _process(_delta: float) -> void:
 	
 	move_and_slide()
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	pass
 
 
--- scripts/octopus.gd
+++ scripts/octopus.gd
@@ -1,11 +1,35 @@
 extends Enemy
 var move_dir = 1;
 
-func _process(delta: float) -> void:
+@onready var raycast = $AnimatedSprite2D/RayCast2D
+
+func _process(_delta: float) -> void:
+	if (!alive):
+		return;
+		
 	#detect cliff and turn around here
+	if !raycast.is_colliding() and is_on_floor():
+		move_dir *= -1; #turn around
+		raycast.target_position.x *= -1;
 	
-	run(move_dir);
 	if (move_dir < 0):
 		face_left()
 	elif (move_dir > 0):
 		face_right();
+		
+	run(move_dir);
+
+
+func _on_hurt_box_body_entered(body):
+	
+	if (body.is_in_group("Player") && alive):
+		set_collision_layer_value(2, false);
+		run(0);
+		velocity.x = 0;
+		alive = false;
+		bounce_character(body)
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == 'death':
+		self.queue_free();
