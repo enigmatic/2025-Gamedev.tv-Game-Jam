da6476ddb1bebdfc52e2a491bc225f3ec04ee545
Matt Couch <mcouch@gmail.com>
2025-05-24 17:21:03 -0500

    add splash

--- scripts/aquarium.gd
+++ scripts/aquarium.gd
@@ -3,6 +3,8 @@ extends Node2D
 signal escaped;
 signal outOfWater;
 signal inWater;
+@onready var outSplash:GPUParticles2D = $outSplash
+@onready var inSplash:GPUParticles2D = $inSplash
 
 func _on_goal_touched(body: Node2D):
 	if body.is_in_group("Player"):
@@ -11,10 +13,14 @@ func _on_goal_touched(body: Node2D):
 
 
 func _on_water_body_entered(body):
+	inSplash.position.x = body.global_position.x
+	inSplash.emitting = true;
 	if body.is_in_group("Player"):
 		inWater.emit();
 
 
-func _on_water_body_exited(body):
+func _on_water_body_exited(body:Node2D):
+	outSplash.position.x = body.global_position.x
+	outSplash.emitting = true;
 	if body.is_in_group("Player"):
 		outOfWater.emit();
